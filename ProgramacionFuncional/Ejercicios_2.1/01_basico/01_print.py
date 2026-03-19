7294fe614a2d669a204df4b014f92e85e10b0025
ALAN-C-AKE <colliakealanalberto@gmail.com>
2026-03-18 20:44:16 -0500

    Primer commit agregando 01_print y 02_types

---
+++ ProgramacionFuncional/Ejercicios_2.1/01_basico/01_print.py
@@ -0,0 +1,24 @@
+#Alan Alberto Colli Ake
+from os import system
+
+if system("clear") !=0: system("cls")
+
+print("Hola, Crayaola")
+
+print('Esto tambien funciona con una comilla')
+
+print("Python", "es", "genial")
+
+print("python", "es", "brutal", sep="-")
+
+print("Esto se imprime", end = "\n")
+print("salto de linea")
+
+print(42)
+
+print('esto es una "pulgada" dentro de un string con comillas simples')
+
+print("esto es una \"pulgada\" dentro de un string con comillas dobles")
+
+print("""esto es una "pulgada" dentro de un string con comillas dobles""")
+
---
+++ ProgramacionFuncional/Ejercicios_2.1/01_basico/02_types.py
@@ -0,0 +1,36 @@
+#Alan Alberto Colli Ake
+from os import system
+if system("clear") !=0: system("cls")
+
+print("int:")   #Enteros
+print(type(10)) #Entero positivo
+print(type(0))  #Cero tambien es entero
+print(type(-5)) #Entero negativo
+print(type(723842472233784278934789239874)) #permite numero enteros de gran tamaño
+
+print("float:")   #decimal flotante
+print(type(3.14)) #punto decimal
+print(type(1.0))  #flotante contiene punto decimal
+print(type(1e3))  #notacion cientifica
+
+print("complex:")
+print(type(1+2j))
+
+print("str:")#cadenas de texto
+print(type("hola"))
+print(type(""))
+print(type("123"))
+print(type("""
+           Multilinea
+           """))#abarca varias lineas
+
+print("bool:") #valores boleanos(true/false)
+print(type(True))
+print(type(False))
+print(type(1<2))
+
+print("NoneType:")#representa ausencia de valor
+print(type(None)) #None es un tipo esécialde dato
+
+
+
